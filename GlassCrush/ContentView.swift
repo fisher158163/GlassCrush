f2880789dbc7e72b8cb5d2ce5bf78af067d12f32
Fisher Lee <fisherlee0820@outlook.com>
2026-03-27 17:58:10 +0800

    Initial Commit

---
+++ GlassCrush/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GlassCrush
+//
+//  Created by liyu on 2026/3/27.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ GlassCrush/GlassCrushApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassCrushApp.swift
+//  GlassCrush
+//
+//  Created by liyu on 2026/3/27.
+//
+
+import SwiftUI
+
+@main
+struct GlassCrushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
